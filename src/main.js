42b21b842f9496a2a8b18bcd492388498f762b92
josseline <josselineacostamtz@gmail.com>
2020-01-14 12:06:29 -0600

    carpetas spa

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { changeView } from './view-controller/route.js';
+
+const init = () => {
+    changeView(window.location.hash);
+    window.addEventListener('hashchange', () => changeView(window.location.hash));
+};
+
+window.addEventListener('load', init);
