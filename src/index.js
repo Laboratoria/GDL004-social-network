34bdf12841a6a7922580680c49f41fc74ec9013c
ItzaCarrillo <vichytacarrillo@gmail.com>
2020-01-13 19:01:05 -0600

    first commit with firebase

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import { example } from './example.js';
 
 example();
+let email = document.getElementById("email_answer").Value
+let password = document.getElementById("paswword_answer")
+
+
+const init = () => {
+    window.addEventListener("hashchange", () => console.log(window.location.hash))
+}
+window.addEventListener('load', init)
