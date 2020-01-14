347f951a3d727f53ea3669e291c5c5f0c09848e5
ItzaCarrillo <vichytacarrillo@gmail.com>
2020-01-14 12:06:31 -0600

    ultimo

--- src/example.js
+++ src/example.js
@@ -1,11 +1,3 @@
-import { example } from './example.js';
-
-example();
-let email = document.getElementById("email_answer").Value
-let password = document.getElementById("paswword_answer")
-
-
-const init = () => {
-    window.addEventListener("hashchange", () => console.log(window.location.hash))
-}
-window.addEventListener('load', init)
+/*export const example = () => {
+    // aquí tu código
+}*/
