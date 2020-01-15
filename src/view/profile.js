7770a94de7e43db5c06b73ffcd7e273616e922f2
josseline <josselineacostamtz@gmail.com>
2020-01-15 12:44:09 -0600

    template profile

--- src/view/profile.js
+++ src/view/profile.js
@@ -1,8 +1,31 @@
 export default () => {
     const viewProfile = `
-  <h2 class="text-center">¡Bienvenido a nuestra página!
-   HOLA SOY EL PROFILE </h2>
-  `;
+    
+    <div class="container_user">
+        <div class="profile_information">
+            <span class="flaticon-051-user"> User </span> 
+        </div>  
+            <div class="profile_description"> Descripción: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices faucibus elit ac rutrum. Donec porttitor tortor aliquam leo pellentesque, pulvinar condimentum sapien condimentum. 
+            </div>
+            <div class="profile_direction"> Dirección: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </div>   
+          <div class="profile_pesornal_information"> Phonee number</div>
+          <div class="flaticon-020-heart profile_icons"> iconos
+          </div>          
+         
+    </div>
+    <div class="container_post">
+    <div class="profile_information">
+        <span class="flaticon-051-user"> User </span> 
+    </div>  
+        <div class="profile_post"> Post: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices faucibus elit ac rutrum. Donec porttitor tortor aliquam leo pellentesque, pulvinar condimentum sapien condimentum. 
+        </div>
+        <div class="profile_post_icons"> 
+        <i class="flaticon-020-heart"></i><i class="flaticon-034-chat"></i><i class="flaticon-018-settings"></i> iconos
+        </div> 
+         
+     
+</div>`;
 
     const divElemt = document.createElement('div');
     
