a423554e561a72d6e20032f20bf31b4476d88a46
ItzaCarrillo <vichytacarrillo@gmail.com>
2020-01-20 18:21:40 -0600

    antes de mvc

--- src/view-controller/route.js
+++ src/view-controller/route.js
@@ -1,19 +1,27 @@
 import { components } from '../view/index.js';
+import  login  from '../view/login.js';
 const changeView = (route) => {
     const container = document.getElementById('container')
         container.innerHTML ="";
-    switch (route ) {
-        
-        case "#/Home":
-             { return container.appendChild( components.home())}
-        case "#/Profile": { return container.appendChild( components.profile())}
-        case "#/EditProfile": { return container.appendChild( components.editProfile())}
+   // const formCreate = document.getElementById('form-signIn');
+   // formCreate.innerHTML ='';
+   // login()
 
-        
+    switch (route ) {
+
+
+        case "#/Home":
+              container.appendChild( components.home())
+        break;
+        case "#/Profile": container.appendChild( components.profile())
+        case "#/EditProfile": container.appendChild( components.editProfile())
+
+
         default:
-            break;
+            { return container.appendChild( components.login())}
     }
-    console.log(route)
+
+    return null;
 }
 
 export {changeView}
