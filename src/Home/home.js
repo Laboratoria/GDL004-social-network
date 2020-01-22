478587eebfad2712db2298f3561a54cca95ccf93
Didre <ceci21062@gmail.com>
2020-01-22 14:00:23 -0600

    Cambio de nombre del archivo de firebase

---
+++ src/Home/home.js
@@ -0,0 +1,14 @@
+/*querySelector's de home*/
+document.querySelector('.about').addEventListener('click', () => {
+    console.log("Este botón mostrará el template de showAbout.");
+    
+     //showAbout();
+  });
+  document.querySelector('.loginUser').addEventListener('click', () => {
+    console.log("Este botón mostrará el template de showLogIn.");
+      //showLogIn();
+  });
+  document.querySelector('.signupUser').addEventListener('click', () => {
+    console.log("Este botón mostrará el template de showSignUp.");
+      //showSignUp();
+  });
