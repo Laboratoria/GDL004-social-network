3fbcca20c5fb6385fe32793432b4fd61d4fe96ed
ItzaCarrillo <vichytacarrillo@gmail.com>
2020-03-30 19:45:52 -0600

    "nuevo archivo firebaseConfig.js que contiene apikey authDomain databaseURL projectId etc de firebase"

--- src/authentication.js
+++
@@ -1,6 +0,0 @@
-function login(){
-  let userEmail = document.getElementById('userEmail').value;
-  let userPass = document.getElementById('userPassword').value;
-
-  window.alert(userEmail + "" userPass);
-}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,23 @@
+
+   
+    
+     const firebaseConfig = {
+       apiKey: "AIzaSyCnQ31KbCMulCxtCuFoBtLaQ3lw76l7wwI",
+       authDomain: "deal-b4259.firebaseapp.com",
+       databaseURL: "https://deal-b4259.firebaseio.com",
+       projectId: "deal-b4259",
+       storageBucket: "deal-b4259.appspot.com",
+       messagingSenderId: "541545225912",
+       appId: "1:541545225912:web:6bc45c07fc362a638b5e4d",
+       measurementId: "G-MS1DQ2F5PX"
+     };
+     // Initialize Firebase
+     firebase.initializeApp(firebaseConfig);
+     window.addEventListener('hashchange', () => {
+       changeView(window.location.hash);
+});
+
+window.addEventListener('load', init);
+
+
+
